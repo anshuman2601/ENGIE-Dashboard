649a487a4ad7a3cce152496b5608f50338a9d6fd
bahaaaldin214 <bahaaaldinmohammad@gmail.com>
2022-10-16 12:41:09 -0500

    final product

--- public/main.js
+++ public/main.js
@@ -1,9 +1,10 @@
 import Data from "/data/index.js"
 
-let input = " 07/03/2021"
-
-const date = new Date(input).toString().slice(0, -42)
-const cached = {};
+const cached = {
+  handled: {},
+  emissions: {}
+};
+let fetched = false;
 
 const Enums = {
   //main purchase to fech factor
@@ -48,21 +49,28 @@ need discussion
 
 const body = document.querySelector(".u-body");
 
-const energyCircle = document.querySelector(".u-shape-2");
-const energyCanvas = document.getElementById('energyCanvas')
+const emissionValue = document.querySelector(".u-text-5");
 const energyValue = document.querySelector(".u-text-4");
 
+const energyCircle = document.querySelector(".u-shape-2");
 const emissionCircle = document.querySelector(".u-shape-1");
-const emissionCanvas = document.getElementById('emissionCanvas')
-const emissionValue = document.querySelector(".u-text-5");
 
-const eContext = energyCanvas.getContext("2d");
-const emContext = emissionCanvas.getContext("2d")
 
-async function main(){
+const searchButton = document.querySelector("#searchButton");
 
-  const emissionChart = {data:[], labels: []};
-  const usageChart = {data:[], labels: []};
+window.onload = async function(){
+  await fetcher();
+  searchButton.disabled = false;
+  console.log("hi")
+  searchButton.onkeydown = function(e){
+    if (e.key !== "Enter") return
+    const date = new Date(searchButton.value).toString().slice(0, -42);
+    main(date);
+  }
+}
+
+
+async function fetcher(){
 
   for (const [key, values] of Object.entries(Enums)){
 
@@ -71,29 +79,64 @@ async function main(){
       await Data.postJson("/main", {name: value}).then(data => {
         const handled = Data.handle(data);
         const emissions = Data.factor(handled, key);
-        cached[value] = handled;
 
-        usageChart.labels.push(value);
-        usageChart.data.push(handled[date]);
-
-        emissionChart.labels.push(value);
-        emissionChart.data.push(emissions[date]);
+        cached.handled[value] = handled;
+        cached.emissions[value] = emissions;
       });
+    }
+  }
+
+}
+
+async function main(date){
+
+  const emissionChart = {data:[], labels: []};
+  const usageChart = {data:[], labels: []};
+
+  energyValue.innerText = 0;
+  emissionValue.innerText = 0;
+
+  for (const [key, values] of Object.entries(Enums)){
+
+    for(const value of values) {
+
+
+      usageChart.labels.push(value);
+      emissionChart.labels.push(value);
+
+      const eValue = cached.handled[value][date];
+      usageChart.data.push(eValue);
+
+      energyValue.innerText = parseInt(energyValue.innerText) + Math.round((eValue || 0)*100)/100;
+      const emValue = cached.emissions[value][date];
+      emissionChart.data.push(emValue);
+
+      emissionValue.innerText = parseInt(emissionValue.innerText) + Math.round((emValue || 0)*100)/100
 
     }
   }
-  energyCircle.style.backgroundColor = "#0000";
-  energyValue.innerText = 100;
 
-  emissionCanvas.style.backgroundColor = "rgba";
-  emissionValue.innerText = 200;
+  const eContext = document.createElement("canvas").getContext("2d");
+  const emContext = document.createElement("canvas").getContext("2d");
+
+  while(energyCircle.firstElementChild){
+    energyCircle.removeChild(energyCircle.firstElementChild);
+  }
+
+  energyCircle.appendChild(eContext.canvas);
+
+  while(emissionCircle.firstElementChild){
+    console.log(emissionCircle, energyCircle)
+    emissionCircle.removeChild(emissionCircle.firstElementChild);
+  }
+
+  emissionCircle.appendChild(emContext.canvas)
+
 
   Data.makeChart(usageChart, eContext);
   Data.makeChart(emissionChart, emContext);
 
 }
-
-window.onload = main;
 
 window.onResize = function(){
   body.height = "100vh"
